2abf64be46432f4ce075cd0c36a86abcf04ebca0
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-20 14:18:36 +0100

    feat: add a uuid field to the team structure

--- src/core/team/model.rs
+++ src/core/team/model.rs
@@ -1,12 +1,20 @@
+use uuid::Uuid;
+
 #[derive(Debug, Clone)]
 pub struct Team {
+  pub uuid: Uuid,
   pub name: String,
   pub reputation: i32,
   pub abbreviation: String,
 }
 
 impl Team {
-  pub fn new(name: String, reputation: i32, abbreviation: String) -> Self {
-    Self { name: name, reputation: reputation, abbreviation: abbreviation }
+  pub fn new(uuid: Uuid, name: String, reputation: i32, abbreviation: String) -> Self {
+    Self { 
+      uuid: uuid, 
+      name: name, 
+      reputation: reputation, 
+      abbreviation: abbreviation 
+    }
   }
 }
