14bf8d88392b2d6b3fc0ff3d780538daf62c5714
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-20 15:43:31 +0100

    feat: create a structure for the states of a team (attack, defence and midfield)

--- src/core/team/model.rs
+++ src/core/team/model.rs
@@ -8,6 +8,13 @@ pub struct Team {
   pub abbreviation: String,
 }
 
+#[derive(Debug, Clone)]
+pub struct TeamStatus {
+  pub attack_overall: i32,
+  pub defense_overall: i32,
+  pub midfield_overall: i32
+}
+
 impl Team {
   pub fn new(uuid: Uuid, name: String, reputation: i32, abbreviation: String) -> Self {
     Self { 
@@ -16,5 +23,11 @@ impl Team {
       reputation: reputation, 
       abbreviation: abbreviation 
     }
+  }
+}
+
+impl TeamStatus {
+  pub fn new_status(attack: i32, defense: i32, midfield: i32) -> Self {
+    Self { attack_overall: attack, defense_overall: defense, midfield_overall: midfield }
   }
 }
